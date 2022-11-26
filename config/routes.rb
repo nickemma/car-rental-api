a59bc1cef01afa5e3309ecc515b3df7440144ae8
amejid <abdelmejid89@gmail.com>
2022-11-26 13:00:01 +0300

    Create destroy routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   post :auth, to: 'authentication#create'
 
   resources :users, only: [:create]
-  resources :cars, except: [:update, :edit, :new]
-resources :reservations, only: [:create, :delete]
-
+  resources :cars, only: [:index, :create, :destroy]
+  resources :reservations, only: [:create, :destroy]
 
 end
