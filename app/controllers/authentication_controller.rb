45ac6f9a0ad582e3d4d7624978a85774434ee0d1
amejid <abdelmejid89@gmail.com>
2022-11-26 14:34:14 +0300

    Add email authentication

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,6 +3,8 @@ class AuthenticationController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
+    return render json: { error: 'Invalid email or password' }, status: :unauthorized unless user
+
     if user.authenticate(params[:password])
       render json: user
     else
