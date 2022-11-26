597f3bd6ad626b630bb629dd8bc7c1b98eb97a8b
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-11-26 11:11:54 +0100

    Update cars controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,26 +1,36 @@
 class CarsController < ApplicationController
+  before_action :check_admin?, only: [:create, :destroy]
+  
   def index
     @cars = Car.all
     render json: @cars
   end
 
-  def show
-    @car = Car.find(params[:id])
-    render json: @car
-  end
-
   def create
     @car = Car.new(car_params)
     if @car.save
-      render json: @car, status: :created
+      render json: @car
     else
-      render json: @car.errors, status: :unprocessable_entity
+      render json: { errors: ['Something went wrong'] }
+    end
+  end
+
+  def destroy
+    @car = Car.find(params[:id])
+    if @car.destroy
+      render json: { id: @car.id, message: 'Car deleted successfully' }
+    else
+      render json: { errors: ['Something went wrong'] }
     end
   end
 
   private
 
   def car_params
-    params.permit(:name, :description, :brand, :daily_rate, :type, :image)
+    params.permit(:name, :description, :brand, :daily_rate, :car_type, :image)
+  end
+
+  def check_admin?
+    @current_user.admin?
   end
 end
