327ebea86fc0a5012e5b3d5c134509172be1110b
amejid <abdelmejid89@gmail.com>
2022-11-28 11:14:51 +0300

    Implement update user data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,15 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = @current_user
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: { error: 'Something went wrong' }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   post :auth, to: 'authentication#create'
 
-  resources :users, only: [:create]
+  resources :users, only: [:create, :update]
   resources :cars, only: [:index, :create, :destroy]
   resources :reservations, only: [:create, :destroy]
 
