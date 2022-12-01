8c4c800bccf6959b6bbe94c5bb0d892ca7482d95
amejid <abdelmejid89@gmail.com>
2022-12-01 17:04:26 +0300

    Add destroy association on models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_one_attached :avatar
 
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
   has_many :cars, through: :reservations
 
   def admin?
