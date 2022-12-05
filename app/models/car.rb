fa259cde538af09304af087c92fcb0bf33e5790a
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-12-05 15:08:43 +0100

    Validate params

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,4 +3,10 @@ class Car < ApplicationRecord
 
   has_many :reservations, dependent: :destroy
   has_many :users, through: :reservations
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :brand, presence: true
+  validates :daily_rate, presence: true, numericality: { greater_than: 0 }
+  validates :car_type, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,11 @@ class User < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_many :cars, through: :reservations
 
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :date_of_birth, presence: true
+
   def admin?
     admin
   end
